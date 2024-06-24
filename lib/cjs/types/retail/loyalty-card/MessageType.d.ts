7d87bb86fb51cd18a3659f9bbce97ef5f72470d5
Clive James Watts <clive@clivewatts.net>
2024-06-24 07:47:49 +0200

    add lib

---
+++ lib/cjs/types/retail/loyalty-card/MessageType.d.ts
@@ -0,0 +1,16 @@
+export declare enum MessageTypeEnum {
+    MESSAGE_TYPE_UNSPECIFIED = "MESSAGE_TYPE_UNSPECIFIED",
+    TEXT = "TEXT",
+    NOTIFY_AND_NOTIFY = "NOTIFY_AND_NOTIFY",
+    /**
+     * This item is deprecated!
+     * @deprecated
+     */
+    text = "text",
+    EXPIRATION_NOTIFICATION = "EXPIRATION_NOTIFICATION",
+    /**
+     * This item is deprecated!
+     * @deprecated
+     */
+    expirationNotification = "expirationNotification"
+}
